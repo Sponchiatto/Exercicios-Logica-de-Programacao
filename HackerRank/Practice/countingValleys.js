ee61eb0c68a2da0dfc8bedc201b2d64e66f36ffc
Sponchiatto <rafael_sponchiatto@hotmail.com>
2024-06-03 10:14:28 -0300

    Counting Valleys - Hackerrank

---
+++ HackerRank/Practice/countingValleys.js
@@ -0,0 +1,40 @@
+// An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly  steps,
+// for every step it was noted if it was an uphill, , or a downhill,  step. Hikes always start and end at
+// sea level, and each step up or down represents a  unit change in altitude. We define the following terms:
+
+// - A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea l
+// evel and ending with a step down to sea level.
+// - A valley is a sequence of consecutive steps below sea level, starting with a step down
+// from sea level and ending with a step up to sea level.
+
+// Given the sequence of up and down steps during a hike, find and print the number of valleys walked through.
+// Example
+// steps = 8 path = [DDUUUUDD]
+
+// The hiker first enters a valley 2 units deep. Then they climb out and up onto a mountain 2 units high.
+// Finally, the hiker returns to sea level and ends the hike.
+
+function countingValleys(steps, path) {
+  let valley = 0;
+  let seaLevel = 0;
+
+  for (let i = 0; i < steps; i++) {
+    if (path[i] === "U") {
+      seaLevel++;
+      if (seaLevel === 0) {
+        valley++;
+      }
+    } else if (path[i] === "D") {
+      seaLevel--;
+    }
+  }
+
+  return valley;
+}
+
+const steps = 8;
+const path = "UDDDUDUU";
+// _/\      _
+//    \    /
+//     \/\/
+console.log(countingValleys(steps, path));
